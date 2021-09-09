a5cba1eb21a5f01f82814bd102950c31035851cc
zhy-stay <57357418+zhy-stay@users.noreply.github.com>
2021-09-09 22:42:10 +0800

    upload

---
+++ Unity-Laboratory/Unity-Laboratory/Assets/02-Timer/PETimer/Scripts/GameRoot.cs
@@ -0,0 +1,16 @@
+/****************************************************
+    文件：GameRoot.cs
+    作者：zhyStay
+    邮箱：zhy18125@163.com
+    日期：#CreateTime#
+    功能：Nothing
+*****************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRoot : MonoBehaviour
+{
+    
+}
