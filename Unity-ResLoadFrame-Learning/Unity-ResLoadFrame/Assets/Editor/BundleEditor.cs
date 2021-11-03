2636342441f43a27259401298a178c59dee2b209
zhy-stay <57357418+zhy-stay@users.noreply.github.com>
2021-11-03 18:24:24 +0800

    upload

---
+++ Unity-ResLoadFrame-Learning/Unity-ResLoadFrame/Assets/Editor/BundleEditor.cs
@@ -0,0 +1,22 @@
+﻿/****************************************************
+    文件：BundleEditor.cs
+    作者：zhyStay
+    邮箱：zhy18125@163.com
+    日期：#CreateTime#
+    功能：Nothing
+*****************************************************/
+
+using UnityEngine;
+using UnityEditor;
+
+public class BundleEditor
+{
+    [MenuItem("Tools/打包AssetBundle")]
+    private static void Build()
+    {
+        // BuildPipeline.BuildAssetBundles(路径, 压缩格式, 打包平台);
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.ChunkBasedCompression, EditorUserBuildSettings.activeBuildTarget);
+        // 刷新编辑器, 使刚刚打包的AB包资源立即导入
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Unity-ResLoadFrame-Learning/Unity-ResLoadFrame/Assets/Test/01-ResourcesLoad/ResourcesLoadTest.cs
@@ -0,0 +1,37 @@
+﻿/****************************************************
+    文件：ResourcesLoadTest.cs
+    作者：zhyStay
+    邮箱：zhy18125@163.com
+    日期：#CreateTime#
+    功能：Nothing
+*****************************************************/
+
+using UnityEngine;
+
+public class ResourcesLoadTest : MonoBehaviour
+{
+    private void Start()
+    {
+        // AB资源加载 
+        //LoadAssetBundleRes("attack", "attack");
+
+        // 在做编辑器扩展时可使用此资源加载方式
+        LoadEditorLoadRes();
+    }
+
+    /// <summary>
+    /// 加载AB包资源
+    /// </summary>
+    /// <param name="fileName">文件名</param>
+    /// <param name="resName">资源名</param>
+    private void LoadAssetBundleRes(string fileName, string resName)
+    {
+        AssetBundle assetBundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/" + fileName);
+        GameObject obj = GameObject.Instantiate(assetBundle.LoadAsset<GameObject>(resName));
+    }
+
+    private void LoadEditorLoadRes()
+    {
+        GameObject obj = GameObject.Instantiate(UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Resources/Prefabs/Attack.prefab"));
+    }
+}
