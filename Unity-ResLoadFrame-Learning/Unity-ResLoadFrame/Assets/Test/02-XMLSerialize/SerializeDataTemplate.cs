87807853ccecb0cc6ac9d404fa6f72d4adafc469
zhy-stay <57357418+zhy-stay@users.noreply.github.com>
2021-11-04 00:04:10 +0800

    upload

---
+++ Unity-ResLoadFrame-Learning/Unity-ResLoadFrame/Assets/Test/02-XMLSerialize/SerializeDataTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+
+[System.Serializable]
+public class SerializeDataTemplate 
+{
+    [XmlAttribute("Id")]
+    public int Id { get; set; }
+
+    [XmlAttribute("Name")]
+    public string Name { get; set; }
+
+    [XmlElement("Score")]
+    public ScoreTemplate Score { get; set; }
+
+    [XmlArray("Hobby")] // [XmlElement("Score")]
+    public List<string> Hobby { get; set; }
+}
+
+[System.Serializable]
+public class ScoreTemplate
+{
+    [XmlAttribute("Mathematics")]
+    public int Mathematics { get; set; }
+
+    [XmlAttribute("English")]
+    public int English { get; set; }
+}
