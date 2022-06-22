df1ecc37cbca45979242cb07744ecfbe7f6a95a0
zhy-stay <57357418+zhy-stay@users.noreply.github.com>
2022-06-22 22:26:44 +0800

    upload

---
+++ Unity-UGUI-Learning/Unity-UGUI/Assets/Scripts/Comments.cs
@@ -0,0 +1,15 @@
+/****************************************************
+	文件：Comments.cs
+	作者：zhystay
+	邮箱：zhy18125@163.com
+	日期：#CreateTime#
+	功能：Nothing
+*****************************************************/
+
+using UnityEngine;
+
+public class Comments : MonoBehaviour
+{
+    [Multiline]
+    public string text;
+}
