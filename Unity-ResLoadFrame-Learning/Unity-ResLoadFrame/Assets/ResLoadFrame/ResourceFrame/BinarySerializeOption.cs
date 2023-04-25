97566ec0e6265c8467cb8a540adabadb861a55d7
zhy-stay-incompany <57357418+Roberzs@users.noreply.github.com>
2023-04-25 18:10:14 +0800

    添加了protobuf解析

--- Unity-ResLoadFrame-Learning/Unity-ResLoadFrame/Assets/ResLoadFrame/ResourceFrame/BinarySerializeOption.cs
+++ Unity-ResLoadFrame-Learning/Unity-ResLoadFrame/Assets/ResLoadFrame/ResourceFrame/BinarySerializeOption.cs
@@ -11,9 +11,72 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
 using UnityEngine;
+using ProtoBuf;
 
 public class BinarySerializeOption
 {
+
+    public static bool ProtoBufSerialize<T>(string path, System.Object obj)
+    {
+        try
+        {
+            using (Stream stream = File.Create(path))
+            {
+                ProtoBuf.Serializer.Serialize(stream, obj);
+                return true;
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Protobuf serialize Error:" + e);
+        }
+        return false;
+    }
+
+    public static System.Object ProtoBufDeserialize(string path, Type type)
+    {
+        System.Object obj = null;
+        try
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+            {
+                obj = Serializer.Deserialize(type, fs);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Protobuf deserialize Error:{path},{e}");
+        }
+        return obj;
+    }
+
+    public static T ProtoBufDeserialize<T>(string path) where T : class
+    {
+        T t = default(T);
+        TextAsset textAsset = ResourceManager.Instance.LoadResource<TextAsset>(path);
+        if (textAsset == null)
+        {
+            Debug.LogError($"cant load TextAsset:{path}");
+            return null;
+        }
+        try
+        {
+            using (MemoryStream stream = new MemoryStream(textAsset.bytes))
+            {
+                //XmlSerializer xs = new XmlSerializer(typeof(T));
+                //t = (T)xs.Deserialize(stream);
+
+                t = Serializer.Deserialize<T>(stream);
+            }
+            ResourceManager.Instance.ReleaseResource(path, true);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"load TextAsset exception:{path},{e}");
+        }
+        return t;
+    }
+
     /// <summary>
     /// XML序列化
     /// </summary>
@@ -54,7 +117,7 @@ public class BinarySerializeOption
     /// <typeparam name="T"></typeparam>
     /// <param name="path"></param>
     /// <returns></returns>
-    public static T XmlDeserializeEditor<T>(string path) where T : class
+    public static T XmlDeserializeInFile<T>(string path) where T : class
     {
         T t = default(T);
         try
